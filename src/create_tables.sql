e0a239404f7556162c77c2072450aa432b09f388
jakedraddy <jakedraddy@noreply.github.com>
2019-03-28 10:48:59 -0400

    Most of create table sql done

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE Person(
     FirstName VARCHAR(30),
     DoB DATETIME,
     SSN VARCHAR(10)
-)
+);
 
 -- PhoneNumber(PId, PersonId, CountryCode, AreaCode, ExchangeCode, LineNumber, Extension)
 CREATE TABLE PhoneNumber(
@@ -44,7 +44,7 @@ CREATE TABLE Email (
 );
 
 -- Employee()
-CREATE TABLE (
+CREATE TABLE Employee (
     EmployeeId INTEGER PRIMARY KEY, 
     FOREIGN KEY PersonId 
         REFERENCES Person(PersonId)
@@ -64,7 +64,7 @@ CREATE TABLE Desk(
     DeskId INTEGER PRIMARY KEY, 
     FloorNumber INTEGER PRIMARY KEY, 
     RoomNumber VARCHAR(6)
-)
+);
 -- Officer
 CREATE TABLE Officer(
     FOREIGN KEY EmployeeId
@@ -73,7 +73,7 @@ CREATE TABLE Officer(
     BadgeId VARCHAR(10)
 
     CONSTRAINT Unique_BadgeId UNIQUE (BadgeId)
-)
+);
 
 -- ForensicExpert(EmployeeId, ForensicExpertId)
 CREATE TABLE ForensicExpert(
@@ -82,19 +82,83 @@ CREATE TABLE ForensicExpert(
         ON DELETE CASCADE, 
     
     ForensicExpertId INTEGER PRIMARY KEY -- todo auto number, oracle does not make this easy like other dbms
-)
+);
 
 
 -- Visit(VisitId, PersonId, Date, Reason)
+CREATE TABLE Visit (
+    VisitId INTEGER PRIMARY KEY, 
+    FOREIGN KEY PersonId REFERENCES Person(PersonId), 
+    "Date" DATETIME, 
+    Reason VARCHAR2(255)
+);
+
 -- Arrest(ArrestNumber, PersonId, BadgeId, Date, ArrestReason)
+CREATE TABLE Arrest (
+    ArrestNumber INTEGER PRIMARY KEY, 
+    FOREIGN KEY PersonId REFERENCES Person(PersonId), 
+    FOREIGN KEY BadgeId REFERNCES Officer(BadgeId), 
+    "Date" DATETIME, 
+    ArrestReason VARCHAR(100)
+);
 
 -- Case(CaseId, DateEntered, Status)
+CREATE TABLE Case (
+    CaseId INTEGER PRIMARY KEY, 
+    DateEntered DATETIME, 
+    Status VARCHAR(10)
+);
+
 -- CaseVisit(CaseId, VisitId)
+CREATE TABLE CaseVisit(
+    FOREIGN KEY CaseId REFERENCES Case(CaseId) PRIMARY KEY,
+    FOREIGN KEY VisitId REFERENCES Visit(VisitId)
+);
+
 -- CaseArrest(CaseId, ArrestNumber)
+CREATE TABLE CaseArrest(
+    ArrestNumber INTEGER PRIMARY KEY,
+    FOREIGN KEY CaseId REFERENCES Case(CaseId)
+);
+
+
 -- CaseAssignments(CaseId, EmployeeId)
+CREATE TABLE CaseAssignments (
+    FOREIGN KEY CaseId REFERENCES Case(CaseId),
+    FOREIGN KEY EmployeeId REFERENCES Employee(EmployeeId),
+    
+    CONSTRAINT PK_CaseAssignments PRIMARY KEY (CaseId, EmployeeId)
+);
 -- CaseNote(NoteId, Note, EmployeeId, DateEntered, CaseId)
+CREATE TABLE CaseNote (
+    NoteId INTEGER PRIMARY KEY,
+    Note VARCHAR(2048),
+    FOREIGN KEY EmployeeId REFERENCES Employee(EmployeeId),
+    DateEntered DATETIME,
+    FOREIGN KEY CaseId REFERENCES Case(CaseId)
+);
 
 -- Evidence(EvidenceId, CaseId, Date, Address, Description, Location)
+CREATE TABLE Evidence(
+    EvidenceId INTEGER PRIMARY KEY, 
+    FOREIGN KEY CaseId REFERENCES Case(CaseId), 
+    "Date" DATETIME, 
+    Address VARCHAR(150), 
+    Description VARCHAR(1024), 
+    Location VARCHAR(150)
+)
+
 
 -- ForensicTest(TestId, EvidenceId, ResultDescription, Date, TestName)
+CREATE TABLE ForensicTest (
+    TestId INTEGER PRIMARY KEY, 
+    FOREIGN KEY EvidenceId REFERENCES Evidence(EvidenceId), 
+    ResultDescription VARCHAR(1024), 
+    "Date" DATETIME,
+    TestName VARCHAR(50)
+)
 -- ForensicTestForensicExpert(TestId, ForensicExpertId)
+CREATE TABLE ForensicTestForensicExpert(
+    FOREIGN KEY TestId REFERENCES FoensicTest(TestId),
+    FOREIGN KEY ForensicExpertId REFERENCES ForensicExpert(ForensicExpertId)
+);
