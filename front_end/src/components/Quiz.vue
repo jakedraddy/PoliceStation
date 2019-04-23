02e147e50f34cf191a1c84b43c5d041efa694454
Patrick Sullivan <pdvortex@gmail.com>
2019-04-23 12:14:28 -0400

    m

--- front_end/src/components/Quiz.vue
+++ front_end/src/components/Quiz.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Quiz</h1>
+    <div class="q-box">
+        <b-jumbotron header="BootstrapVue" lead="Bootstrap 4 Components for Vue.js 2">
+            <p>For more information visit website</p>
+            <b-button variant="primary" href="#">More Info</b-button>
+        </b-jumbotron>
+    </div>
 </template>
