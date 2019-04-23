1cab3925c66f8dbf6a61586db5b62b80729e7c9f
jakedraddy <jakedraddy@noreply.github.com>
2019-04-23 15:43:31 -0400

    fix extra line

--- front_end/src/main.ts
+++ front_end/src/main.ts
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-Vue.http
-Vue.http.headers.common['Access-Control-Allow-Origin'] = '*'
 
 new Vue({
   render: h => h(App)
